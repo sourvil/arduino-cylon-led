d0267ee8d3229927b3b5660cc7bd137d6b44639b
Burak Donbay <burak.donbay@gmail.com>
2016-09-04 22:55:43 +0300

    initial

---
+++ index.js
@@ -0,0 +1,20 @@
+var Cylon = require('cylon');
+ 
+// define the robot 
+var robot = Cylon.robot({
+  // change the port to the correct one for your Arduino 
+  connections: {
+    arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+  },
+ 
+  devices: {
+    led: { driver: 'led', pin: 13 }
+  },
+ 
+  work: function(my) {
+    every((1).second(), my.led.toggle);
+  }
+});
+ 
+// connect to the Arduino and start working 
+robot.start();
